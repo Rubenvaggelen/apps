b7e1217f79d6b6dde95dccd7331deda12720ab01
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-06 21:21:05 +0000

    Build-fix + automatisch opschonen van tegels voor gedeïnstalleerde apps

---
+++ the-one-merged/app/src/main/java/com/gmailorg/hub/HomeTile.kt
@@ -0,0 +1,10 @@
+package com.gmailorg.hub
+
+data class HomeTile(
+    val id: String,               // "notifications" | "mail" | package name van een app
+    val type: TileType,
+    val label: String,
+    val packageName: String? = null // alleen voor type APP
+)
+
+enum class TileType { NOTIFICATIONS, MAIL, HOUSEHOLD, MOVIES, PARKING, SETTINGS, APP, ADD_BUTTON }
