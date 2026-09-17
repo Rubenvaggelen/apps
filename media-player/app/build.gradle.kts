016c792e27425d575dc4cf24e5eaa7d21c1e82c2
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-18 00:02:54 +0200

    Bump Media Player startup hotfix version

--- media-player/app/build.gradle.kts
+++ media-player/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.theone.mediaplayer"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.3.0-test"
+        versionCode = 4
+        versionName = "0.3.1-hotfix"
     }
 
     compileOptions {
