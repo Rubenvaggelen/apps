af70a2beef4a954a56ab5b6132d39e27c168543c
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-18 20:20:16 +0200

    Prepare Media Player v13 YT search list

--- media-player/app/build.gradle.kts
+++ media-player/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.theone.mediaplayer"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1200
-        versionName = "1.2.0-private"
+        versionCode = 1300
+        versionName = "1.3.0-private"
     }
 
     signingConfigs {
