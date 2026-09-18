ef4d42d4210bcec47be353ed16f3f08dd1c743fa
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-18 19:44:16 +0200

    Prepare Media Player v8 Xtream build

--- media-player/app/build.gradle.kts
+++ media-player/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.theone.mediaplayer"
         minSdk = 23
         targetSdk = 36
-        versionCode = 700
-        versionName = "0.7.0-private"
+        versionCode = 800
+        versionName = "0.8.0-private"
     }
 
     signingConfigs {
