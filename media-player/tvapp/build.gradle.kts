7987097806bfa6bd80ae35077f274172a536d131
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-19 12:26:57 +0200

    Add Android TV Media Player build

---
+++ media-player/tvapp/build.gradle.kts
@@ -0,0 +1,43 @@
+plugins {
+    id("com.android.application")
+}
+
+android {
+    namespace = "com.theone.mediaplayer"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.theone.mediaplayer.tv"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1001
+        versionName = "1.0.1-tv"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    sourceSets {
+        getByName("main") {
+            java.srcDirs("../app/src/main/java")
+            res.srcDirs("../app/src/main/res", "src/main/res")
+            assets.srcDirs("../app/src/main/assets")
+            manifest.srcFile("src/main/AndroidManifest.xml")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.media3:media3-exoplayer:1.11.0")
+    implementation("androidx.media3:media3-exoplayer-hls:1.11.0")
+    implementation("androidx.media3:media3-exoplayer-dash:1.11.0")
+    implementation("androidx.media3:media3-ui:1.11.0")
+}
