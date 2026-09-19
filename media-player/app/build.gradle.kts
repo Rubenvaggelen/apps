8bcd701e5603416b1c06124b7dfa596223dba841
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-19 15:05:08 +0200

    Bump Media Player phone for phone streaming

--- media-player/app/build.gradle.kts
+++ media-player/app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.theone.mediaplayer"
         minSdk = 23
         targetSdk = 36
-        versionCode = 2202
-        versionName = "2.2.2-phone-complete"
+        versionCode = 2203
+        versionName = "2.2.3-phone-cast"
     }
 
     signingConfigs {
