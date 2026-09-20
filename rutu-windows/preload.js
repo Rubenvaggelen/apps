c05b20b4d575d06dbc58ad1c5791a0baf54ae164
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-20 23:32:02 +0200

    Expose Rutu Windows online order refresh

--- rutu-windows/preload.js
+++ rutu-windows/preload.js
@@ -5,6 +5,7 @@ contextBridge.exposeInMainWorld('rutu', {
   openBusiness: () => ipcRenderer.send('open-business'),
   getServerInfo: () => ipcRenderer.invoke('get-server-info'),
   getOrders: () => ipcRenderer.invoke('get-orders'),
+  syncOrders: () => ipcRenderer.invoke('sync-cloud-orders'),
   placeOrder: order => ipcRenderer.invoke('place-order', order),
   setStatus: (id, status) => ipcRenderer.invoke('set-status', { id, status }),
   resetOrders: () => ipcRenderer.invoke('reset-orders'),
