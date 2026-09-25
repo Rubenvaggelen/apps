0a14eb646c31aeef8479855daee5ba07735432a1
Rubenvaggelen <ruben.vanaggelen@gmail.com>
2026-09-25 19:26:00 +0200

    Notify user after The One Windows update completes

--- windows/the-one-main/App.xaml.cs
+++ windows/the-one-main/App.xaml.cs
@@ -11,6 +11,8 @@ public partial class App : Application
         MainWindow = window;
         window.Show();
 
+        WindowsUpdateService.ShowCompletedUpdateIfNeeded(window);
+
         // Windows heeft een volledig eigen updatekanaal. Dit raakt The One
         // Android en The One Car niet.
         _ = WindowsUpdateService.CheckForUpdateAsync(window, silentIfCurrent: true);
